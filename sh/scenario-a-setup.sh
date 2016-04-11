5caeaf17b7b06d6f993bc89c0f6a8ca12841b12e
Jenkins Veyron <jenkins.veyron.rw@gmail.com>
2016-04-11 10:16:25 -0700

    pull 0b08d596d3939d4c9148aec20ebb824647af1ee4

--- sh/scenario-a-setup.sh
+++ sh/scenario-a-setup.sh
@@ -7,26 +7,27 @@ echo "Block 'envVars' (1/1 in completer) of content/tutorials/setup.md"
 # If JIRI_ROOT or VANADIUM_RELEASE are not defined, set them to the default values
 # from the installation instructions and hope for the best.
 
-[ -z "$JIRI_ROOT" ] && export JIRI_ROOT=${HOME}/vanadium
-[ -z "$VANADIUM_RELEASE" ] && export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
+[ -z "${JIRI_ROOT}" ] && export JIRI_ROOT=${HOME}/vanadium
+[ -z "${VANADIUM_RELEASE}" ] && export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
 
 # All files created by the tutorial will be placed in $V_TUT. It is a disposable
 # workspace, easy to recreate.
 export V_TUT=${V_TUT-$HOME/v23_tutorial}
 
-# V_BIN is a convenience for running Vanadium binaries. It avoids the need to
-# modify your PATH or to be 'in' a particular directory when doing the
-# tutorials.
+# V_BIN is the location for Vanadium binaries.
 export V_BIN=${VANADIUM_RELEASE}/bin
+
+# Include the Vanadium binaries in the PATH.
+export PATH=${V_BIN}:${PATH}
 
 # For the shell doing the tutorials, GOPATH must include both Vanadium and the
 # code created as a result of doing the tutorials. To avoid trouble with
 # accumulation, $GOPATH is intentionally omitted from the right hand side (any
 # existing value is ignored).
-if [ -z "$V23_GOPATH" ]; then
+if [ -z "${V23_GOPATH}" ]; then
   export V23_GOPATH=`${JIRI_ROOT}/.jiri_root/scripts/jiri go env GOPATH`
 fi
-export GOPATH=$V_TUT:${V23_GOPATH}
+export GOPATH=${V_TUT}:${V23_GOPATH}
 
 # HISTCONTROL set as follows excludes long file creation commands used in
 # tutorials from your shell history.
@@ -59,26 +60,27 @@ echo "Block 'envVars' (1/1 in completer) of content/tutorials/setup.md"
 # If JIRI_ROOT or VANADIUM_RELEASE are not defined, set them to the default values
 # from the installation instructions and hope for the best.
 
-[ -z "$JIRI_ROOT" ] && export JIRI_ROOT=${HOME}/vanadium
-[ -z "$VANADIUM_RELEASE" ] && export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
+[ -z "${JIRI_ROOT}" ] && export JIRI_ROOT=${HOME}/vanadium
+[ -z "${VANADIUM_RELEASE}" ] && export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
 
 # All files created by the tutorial will be placed in $V_TUT. It is a disposable
 # workspace, easy to recreate.
 export V_TUT=${V_TUT-$HOME/v23_tutorial}
 
-# V_BIN is a convenience for running Vanadium binaries. It avoids the need to
-# modify your PATH or to be 'in' a particular directory when doing the
-# tutorials.
+# V_BIN is the location for Vanadium binaries.
 export V_BIN=${VANADIUM_RELEASE}/bin
+
+# Include the Vanadium binaries in the PATH.
+export PATH=${V_BIN}:${PATH}
 
 # For the shell doing the tutorials, GOPATH must include both Vanadium and the
 # code created as a result of doing the tutorials. To avoid trouble with
 # accumulation, $GOPATH is intentionally omitted from the right hand side (any
 # existing value is ignored).
-if [ -z "$V23_GOPATH" ]; then
+if [ -z "${V23_GOPATH}" ]; then
   export V23_GOPATH=`${JIRI_ROOT}/.jiri_root/scripts/jiri go env GOPATH`
 fi
-export GOPATH=$V_TUT:${V23_GOPATH}
+export GOPATH=${V_TUT}:${V23_GOPATH}
 
 # HISTCONTROL set as follows excludes long file creation commands used in
 # tutorials from your shell history.
