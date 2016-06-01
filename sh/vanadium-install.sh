365d4ea3a11b9a9fa1b682b65c737e5590cb56f7
Jenkins Veyron <jenkins.veyron.rw@gmail.com>
2016-06-01 15:21:51 -0700

    pull 80ba7fbb01ad70c80e9e813ccc7a5031f3064b57

--- sh/vanadium-install.sh
+++ sh/vanadium-install.sh
@@ -2,13 +2,13 @@
 # Script @test from content/installation/step-by-step.md 
 #
 #----------------------------------------------------------------------#  Start 1
-echo "Block 'checkForBash' (1/9 in test) of content/installation/step-by-step.md"
+echo "Block 'checkForBash' (1/8 in test) of content/installation/step-by-step.md"
 ####
 set | grep BASH > /dev/null || echo "Vanadium installation requires Bash."
 #----------------------------------------------------------------------#  End 1
 
 #----------------------------------------------------------------------#  Start 2
-echo "Block 'define_JIRI_ROOT' (2/9 in test) of content/installation/step-by-step.md"
+echo "Block 'define_JIRI_ROOT' (2/8 in test) of content/installation/step-by-step.md"
 ####
 # Uses existing $JIRI_ROOT environment variable, defaults to ${HOME}/vanadium if
 # $JIRI_ROOT is not set.
@@ -16,7 +16,7 @@ export JIRI_ROOT=${JIRI_ROOT:=${HOME}/vanadium}
 #----------------------------------------------------------------------#  End 2
 
 #----------------------------------------------------------------------#  Start 3
-echo "Block 'define_VANADIUM_RELEASE' (3/9 in test) of content/installation/step-by-step.md"
+echo "Block 'define_VANADIUM_RELEASE' (3/8 in test) of content/installation/step-by-step.md"
 ####
 # Needed for tutorials only.
 export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
@@ -33,13 +33,13 @@ trap handledTrouble INT TERM
 # Script @test from content/installation/step-by-step.md 
 #
 #----------------------------------------------------------------------#  Start 1
-echo "Block 'checkForBash' (1/9 in test) of content/installation/step-by-step.md"
+echo "Block 'checkForBash' (1/8 in test) of content/installation/step-by-step.md"
 ####
 set | grep BASH > /dev/null || echo "Vanadium installation requires Bash."
 #----------------------------------------------------------------------#  End 1
 
 #----------------------------------------------------------------------#  Start 2
-echo "Block 'define_JIRI_ROOT' (2/9 in test) of content/installation/step-by-step.md"
+echo "Block 'define_JIRI_ROOT' (2/8 in test) of content/installation/step-by-step.md"
 ####
 # Uses existing $JIRI_ROOT environment variable, defaults to ${HOME}/vanadium if
 # $JIRI_ROOT is not set.
@@ -47,14 +47,14 @@ export JIRI_ROOT=${JIRI_ROOT:=${HOME}/vanadium}
 #----------------------------------------------------------------------#  End 2
 
 #----------------------------------------------------------------------#  Start 3
-echo "Block 'define_VANADIUM_RELEASE' (3/9 in test) of content/installation/step-by-step.md"
+echo "Block 'define_VANADIUM_RELEASE' (3/8 in test) of content/installation/step-by-step.md"
 ####
 # Needed for tutorials only.
 export VANADIUM_RELEASE=${JIRI_ROOT}/release/go
 #----------------------------------------------------------------------#  End 3
 
 #----------------------------------------------------------------------#  Start 4
-echo "Block 'check_JIRI_ROOT' (4/9 in test) of content/installation/step-by-step.md"
+echo "Block 'check_JIRI_ROOT' (4/8 in test) of content/installation/step-by-step.md"
 ####
 # Check that the JIRI_ROOT path does not exist.
 if [[ -e "${JIRI_ROOT}" ]]; then
@@ -70,40 +70,30 @@ fi
 #----------------------------------------------------------------------#  End 4
 
 #----------------------------------------------------------------------#  Start 5
-echo "Block 'runBootstrapScript' (5/9 in test) of content/installation/step-by-step.md"
+echo "Block 'runBootstrapScript' (5/8 in test) of content/installation/step-by-step.md"
 ####
 # This can take several minutes.
 curl -f https://vanadium.github.io/bootstrap.sh | bash
 #----------------------------------------------------------------------#  End 5
 
 #----------------------------------------------------------------------#  Start 6
-echo "Block 'addDevtoolsToPath' (6/9 in test) of content/installation/step-by-step.md"
+echo "Block 'addDevtoolsToPath' (6/8 in test) of content/installation/step-by-step.md"
 ####
 export PATH=$JIRI_ROOT/.jiri_root/scripts:$PATH
 #----------------------------------------------------------------------#  End 6
 
 #----------------------------------------------------------------------#  Start 7
-echo "Block 'packagesBaseProfile' (7/9 in test) of content/installation/step-by-step.md"
+echo "Block 'installBaseProfile' (7/8 in test) of content/installation/step-by-step.md"
 ####
-# Print the package installation command.
-jiri profile os-packages v23:base
-
-# Run the package installation command as root.
-sudo $(jiri profile os-packages v23:base)
+jiri profile install v23:base
 #----------------------------------------------------------------------#  End 7
 
 #----------------------------------------------------------------------#  Start 8
-echo "Block 'installBaseProfile' (8/9 in test) of content/installation/step-by-step.md"
-####
-jiri profile install v23:base
-#----------------------------------------------------------------------#  End 8
-
-#----------------------------------------------------------------------#  Start 9
-echo "Block 'installVanadiumBinaries' (9/9 in test) of content/installation/step-by-step.md"
+echo "Block 'installVanadiumBinaries' (8/8 in test) of content/installation/step-by-step.md"
 ####
 # Install specific tools needed for the tutorials.
 jiri go install v.io/x/ref/cmd/... v.io/x/ref/services/agent/... v.io/x/ref/services/mounttable/...
-#----------------------------------------------------------------------#  End 9
+#----------------------------------------------------------------------#  End 8
 
 echo " "
 echo "All done.  No errors."
